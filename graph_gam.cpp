31ced86fc808565d1b18c1e3fcb6cd9298502c79
grftwhre <44138690+grftwhre@users.noreply.github.com>
2019-10-25 20:03:39 +0300

    Create graph_gam.cpp

---
+++ graph_gam.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+void swap_string(queue A,int k,int i)
+{
+for (int j=0,j<i-k;j++)
+swap(queue.el(j),queue.el(k-j))
+}
+
+int f(queue.el(a),queue.el(b))
+{
+if ((G[a][b]==0)&(G[a][b]==0))
+return 0;
+if ((G[a][b]!=0)&(G[a][b]!=0))
+return 1;
+}
+int main() {
+int N;
+int G[N][N];
+printf (“enter N”);
+scanf(“%d”,&N);
+for (int i=0;i<N;i++)
+{
+for (int j=0;j<N;j++)
+{
+printf(“G[i][j] = “);
+scanf(“%d”,&G[i][j]);
+}
+}
+queue A; //очередь вершин//
+int k=n-1;
+while(k)
+{
+if (f(queue.el(0),queue.el(1))!=0) //проверка есть ли ребро между 0 и 1 вершиной//
+
+while((f(queue.el(0),queue.el(i))!=0)||(f(queue.el(1),queue.el(i+1))!=0))
+i++;
+
+swap_string(1,i); //переворачиваем перестановку от 1 до i//
+queue.push_back(queue.top()); //добавление в конец первой вершины//
+queue.pop(); //удаление вершины//
+k—;
+}
+} 
