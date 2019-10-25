89b60c1b760355c7c9774dff0fc8dbfb82deaeff
grftwhre <44138690+grftwhre@users.noreply.github.com>
2019-10-25 21:14:14 +0300

    Update graph_gam.cpp

--- graph_gam.cpp
+++ graph_gam.cpp
@@ -33,10 +33,10 @@ while(k)
 {
 if (f(queue.el(0),queue.el(1))!=0) //проверка есть ли ребро между 0 и 1 вершиной//
 
-while((f(queue.el(0),queue.el(i))!=0)||(f(queue.el(1),queue.el(i+1))!=0))
+while((f(queue.el(0),queue.el(i))!=0)&&(f(queue.el(1),queue.el(i+1))!=0))
 i++;
 
-swap_string(1,i); //переворачиваем перестановку от 1 до i//
+swap_string(queue A,1,i); //переворачиваем перестановку от 1 до i//
 queue.push_back(queue.top()); //добавление в конец первой вершины//
 queue.pop(); //удаление вершины//
 k—;
