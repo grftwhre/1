e3343723eb8e89b772591c0b3316881f4a63087d
grftwhre <44138690+grftwhre@users.noreply.github.com>
2019-10-25 23:03:11 +0300

    Update graph_gam.cpp

--- graph_gam.cpp
+++ graph_gam.cpp
@@ -4,10 +4,10 @@ using namespace std;
 void swap_string(queue A,int k,int i)
 {
 for (int j=0,j<i-k;j++)
-swap(queue.el(j),queue.el(k-j))
+swap(A.at(j),A.at(k-j))
 }
 
-int f(A.at(a),A.at(b))
+int f(A.at(a),A.at(b),G)
 {
 if ((G[a][b]==0)&(G[b][a]==0))
 return 0;
@@ -33,13 +33,13 @@ queue <int>A; //очередь вершин//
 int k=n-1;
 while(k)
 {
-if (f(A.at(0),A.at(1))!=0) //проверка есть ли ребро между 0 и 1 вершиной//
+if (f(A.at(0),A.at(1),G)!=0) //проверка есть ли ребро между 0 и 1 вершиной//
 
-while((f(A.at(0),A.at(i))!=0)&&(f(A.at(1),A.at(i+1))!=0))
+while((f(A.at(0),A.at(i),G)!=0)&&(f(A.at(1),A.at(i+1),G)!=0))
 i++;
 
 swap_string(A,1,i); //переворачиваем перестановку от 1 до i//
-queue.push_back(A.top()); //добавление в конец первой вершины//
+A.push_back(A.top()); //добавление в конец первой вершины//
 A.pop(); //удаление вершины//
 k—;
 }
